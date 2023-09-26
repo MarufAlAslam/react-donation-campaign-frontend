e4e9eea689ee65273f13de48de214136f5e0c9e1
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-09-26 09:14:22 +0600

    vite initiated

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+import './assets/styles/tailwind.config.css'
+import './assets/styles/globals.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  "Hello"
+)
