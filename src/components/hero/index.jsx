a742bfabd05680237a1ab1683fcd49f7b9b460a1
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-09-26 09:43:07 +0600

    hero done

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,9 +1,32 @@
-const Hero = () => {
-    return (
-        <div className="hero">
-            <div className="container"></div>
-        </div>
-    )
-}
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import Search from "../search";
 
-export default Hero
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="container">
+        <div className="flex justify-between items-center gap-3">
+          <NavLink to="/" className="text-2xl font-bold text-black">
+            <img src={logo} className="logo w-[160px]" alt="" />
+          </NavLink>
+          <div className="flex justify-end items-center gap-[50px] menu">
+            <NavLink to="/" className="text-lg text-[#0B0B0B]">
+              Home
+            </NavLink>
+            <NavLink to="/donation" className="text-lg text-[#0B0B0B]">
+              Donation
+            </NavLink>
+            <NavLink to="/statistics" className="text-lg text-[#0B0B0B]">
+              Statistics
+            </NavLink>
+          </div>
+        </div>
+
+        <Search/>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/search/index.jsx
@@ -0,0 +1,26 @@
+const Search = () => {
+  return (
+    <div className="search py-[120px]">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-[#0B0B0B] mb-[40px]">
+          I Grow By Helping People In Need
+        </h1>
+
+        <form action="">
+          <input
+            type="text"
+            placeholder="Search here..."
+            className="border text-sm border-[#FF444A] rounded-l-[10px] py-[16px] px-[16px] w-[400px] focus:outline-none"
+          />
+          <input
+            type="submit"
+            value="Search"
+            className="bg-[#FF444A] border text-sm border-[#FF444A] text-white rounded-r-[10px] py-[16px] px-[30px] cursor-pointer"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
